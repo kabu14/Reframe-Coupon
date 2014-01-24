bb35e35330b39e611ec627f4e6217c5c54bcefd0
Wayne MacAir <liquidalloy@gmail.com>
2014-01-24 15:38:49 -0800

    additional info added to readme

--- core/init-coupon.php
+++ core/init-coupon.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+require '../data.php';
 require 'connect/database.php';
 require 'classes/users.php';
 require 'classes/general.php';
@@ -8,7 +9,7 @@ require 'captcha/recaptchalib.php';
 require 'FPDI/fpdf.php'; 
 require 'FPDI/fpdi.php';
 
-$privatekey = "6Leuhu0SAAAAAFFiBOMhDnGNVya5gH0GSAtgMBVF";
+
 $resp = recaptcha_check_answer ($privatekey,
                                 $_SERVER["REMOTE_ADDR"],
                                 $_POST["recaptcha_challenge_field"],
