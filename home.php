2c74632ec0fa15dfc2134b2f7216c0152c9fea48
Wayne MacAir <liquidalloy@gmail.com>
2014-01-23 16:34:44 -0800

    logout function

--- home.php
+++ home.php
@@ -1,1 +1,21 @@
-Logged in!
+<?php 
+require 'core/init.php';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css" >
+	<title>Home</title>
+</head>
+<body>	
+	<div id="container">
+		<ul>
+ 
+			<li><a href="index.php">Home</a></li>
+			<li><a href="logout.php">Logout</a></li>
+ 
+		</ul>
+	</div>
+</body>
+</html>
